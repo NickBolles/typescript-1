a0703d1397d5181ff4cffb5b1270592dbd2cf2a6
Nick Bolles <me@nickbolles.com>
2019-10-24 23:00:37 -0500

    add vue module typings

--- packages/types/index.d.ts
+++ packages/types/index.d.ts
@@ -2,3 +2,8 @@ import './process'
 
 export { Context, Middleware, NuxtAppOptions, NuxtError, Plugin, Transition } from './app'
 export { Configuration, Module, ServerMiddleware } from './config'
+
+declare module '*.vue' {
+  import Vue from 'vue'
+  export default Vue
+}
